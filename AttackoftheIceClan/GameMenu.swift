dee68404ad997e488d7461ad15e251bbea5d992e
Adam Saxton <ams17a@acu.edu>
2019-04-29 16:03:57 -0500

    Added things to menu, fixed splash screen, and added two images

--- AttackoftheIceClan/GameMenu.swift
+++ AttackoftheIceClan/GameMenu.swift
@@ -11,10 +11,33 @@ import SpriteKit
 
 class GameMenuScene: SKScene {
     let background = SKSpriteNode(imageNamed: "SplashBack")
+    let title = SKLabelNode(fontNamed: "Rockwell Bold")
+    let play = SKSpriteNode(imageNamed: "play")
+    let shop = SKSpriteNode(imageNamed: "shop")
+    
     
     override func didMove(to view: SKView) {
-        let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
-        let gameScene = GameScene(size: self.size)
-        view.presentScene(gameScene, transition: reveal)
+        //Add background to center of the screen
+        background.zPosition = 0
+        background.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+        addChild(background)
+        
+        //Add title
+        title.text = "Attack of the Ice Clan!"
+        title.fontColor = UIColor.black
+        title.fontSize = 40
+        title.zPosition = 1
+        title.position = CGPoint(x: frame.size.width/2, y: (frame.size.height/4)*3)
+        addChild(title)
+        
+        //Add play button
+        play.zPosition = 1
+        play.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+        addChild(play)
+        
+        //Add shop button
+        shop.zPosition = 1
+        shop.position = CGPoint(x: frame.size.width/2, y: frame.size.height/3)
+        addChild(shop)
     }
 }
