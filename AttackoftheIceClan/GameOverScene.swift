ab21ed0726bb205db1f449323f1f79fce47e92f4
Adam Saxton <ams17a@acu.edu>
2019-04-30 17:02:52 -0500

    Made GameOver scene interactive and made assists for playAgain and mainMenu buttons.

--- AttackoftheIceClan/GameOverScene.swift
+++ AttackoftheIceClan/GameOverScene.swift
@@ -10,37 +10,61 @@ import Foundation
 import SpriteKit
 
 class GameOverScene: SKScene {
+    
+    // Initialize buttons
+    let playAgain = SKSpriteNode(imageNamed: "playAgain")
+    let mainMenu = SKSpriteNode(imageNamed: "mainMenu")
+    
     init(size: CGSize, won:Bool) {
         super.init(size: size)
         
-        // 1
+        // Make Background color white
         backgroundColor = SKColor.white
         
-        // 2
+        // Set display message based on if player won or lost
         let message = won ? "You have defeated the Ice Clan!" : "The Ice Clan has won. :("
         
-        // 3
+        // Set a label to the messsage
         let label = SKLabelNode(fontNamed: "Chalkduster")
         label.text = message
         label.fontSize = 40
         label.fontColor = SKColor.black
-        label.position = CGPoint(x: size.width/2, y: size.height/2)
+        label.position = CGPoint(x: size.width/2, y: (size.height/4)*3)
         addChild(label)
         
-        // 4
-        run(SKAction.sequence([
-            SKAction.wait(forDuration: 3.0),
-            SKAction.run() { [weak self] in
-                // 5
-                guard let `self` = self else { return }
-                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
-                let scene = GameScene(size: size)
-                self.view?.presentScene(scene, transition:reveal)
-            }
-            ]))
+        // Add "Play Again?" button
+        playAgain.zPosition = 1
+        playAgain.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(playAgain)
+        
+        // Add "Main Menu" button
+        mainMenu.zPosition = 1
+        mainMenu.position = CGPoint(x: size.width/2, y: size.height/3)
+        addChild(mainMenu)
+        
+        // make buttons clickable and go to a new scene
+//
+        }
+    
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let touchLocation = touch?.location(in: self)
+        
+        if playAgain.contains(touchLocation!) {
+            let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+            let scene = GameScene(size: (self.view?.bounds.size)!)
+            self.view?.presentScene(scene, transition:reveal)
+        }
+        
+        if mainMenu.contains(touchLocation!) {
+            let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+            let scene = GameMenuScene(size: (self.view?.bounds.size)!)
+            self.view?.presentScene(scene, transition:reveal)
+        }
     }
     
-    // 6
+    // Run error if something happens requiring init
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
---
+++ AttackoftheIceClan/ShopScene.swift
@@ -0,0 +1,14 @@
+//
+//  ShopScene.swift
+//  AttackoftheIceClan
+//
+//  Created by Adam Saxton on 4/30/19.
+//  Copyright © 2019 Adam Saxton. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class ShopScene: SKScene {
+    
+}
