c35383836265a4ced52388b526cfb8aaf8b0329d
Adam Saxton <ams17a@acu.edu>
2019-05-23 14:42:34 -0500

    Added radius upgrade to Upgrade class.

--- AttackoftheIceClan/Upgrade.swift
+++ AttackoftheIceClan/Upgrade.swift
@@ -15,17 +15,21 @@ class Upgrade {
     
     // MARK: Properties
     
-    private var piercing: Int?
-    
-    private var bullets: Int?
-    
     private var defaults = UserDefaults.standard
     
     private var coins: Int?
     
+    private var piercing: Int?
+    
+    private var piercingCost: Int?
+    
+    private var bullets: Int?
+    
     private var bulletCost: Int?
     
-    private var piercingCost: Int? 
+    private var radius: Int?
+    
+    private var radiusCost: Int?
     
     // MARK: Methods
     
@@ -33,10 +37,11 @@ class Upgrade {
     init() {
         
         retrieveData()
-        if (coins == 0 && bulletCost == 0 && piercingCost == 0) {
+        if (coins == 0 && piercingCost == 0 && bulletCost == 0 && radiusCost == 0) {
             coins = 0
-            bulletCost = 50
             piercingCost = 50
+            bulletCost = 50
+            radiusCost = 50
             saveData()
         }
         
@@ -45,11 +50,13 @@ class Upgrade {
     /** Retrieve data from persistent store */
     private func retrieveData() {
         
-        piercing = defaults.integer(forKey: "piercing")
-        bullets = defaults.integer(forKey: "bullets")
         coins = defaults.integer(forKey: "coins")
+        bullets = defaults.integer(forKey: "bullets")
         bulletCost = defaults.integer(forKey: "bulletCost")
+        piercing = defaults.integer(forKey: "piercing")
         piercingCost = defaults.integer(forKey: "piercingCost")
+        radius = defaults.integer(forKey: "radius")
+        radiusCost = defaults.integer(forKey: "radiusCost")
         
     }
     
@@ -62,17 +69,19 @@ class Upgrade {
         defaults.set(coins, forKey: "coins")
         defaults.set(bulletCost, forKey: "bulletCost")
         defaults.set(piercingCost, forKey: "piercingCost")
+        defaults.set(radius, forKey: "radius")
+        defaults.set(radiusCost, forKey: "radiusCost")
         
     }
     
     // MARK: Getters
     
-    /** Get number of bullets that can be shot at once */
-    func bulletCount() -> Int {
+    // Get number of coins
+    func coinCount() -> Int {
         
         retrieveData()
-        if let bullets = bullets {
-            return bullets
+        if let coins = coins {
+            return coins
         } else {
             return 0
         }
@@ -91,31 +100,7 @@ class Upgrade {
         
     }
     
-    // Get number of coins that can be shot at once
-    func coinCount() -> Int {
-        
-        retrieveData()
-        if let coins = coins {
-            return coins
-        } else {
-            return 0
-        }
-        
-    }
-    
-    // Get number of coins that can be shot at once
-    func bulletCostCount() -> Int {
-        
-        retrieveData()
-        if let bulletCost = bulletCost {
-            return bulletCost
-        } else {
-            return 50
-        }
-        
-    }
-    
-    // Get number of coins that can be shot at once
+    // Get cost for piercing
     func piercingCostCount() -> Int {
         
         retrieveData()
@@ -127,12 +112,60 @@ class Upgrade {
         
     }
     
+    /** Get number of bullets that can be shot at once */
+    func bulletCount() -> Int {
+        
+        retrieveData()
+        if let bullets = bullets {
+            return bullets
+        } else {
+            return 0
+        }
+        
+    }
+    
+    // Get cost for bullets
+    func bulletCostCount() -> Int {
+        
+        retrieveData()
+        if let bulletCost = bulletCost {
+            return bulletCost
+        } else {
+            return 50
+        }
+        
+    }
+    
+    // Get number of radius that a bullet can affect
+    func radiusCount() -> Int {
+        
+        retrieveData()
+        if let radius = radius {
+            return radius
+        } else {
+            return 0
+        }
+        
+    }
+    
+    // Get cost for radius
+    func radiusCostCount() -> Int {
+        
+        retrieveData()
+        if let radiusCost = radiusCost {
+            return radiusCost
+        } else {
+            return 50
+        }
+        
+    }
+    
     // MARK: Incrementers
     
-    /** Increment the player's number of bullets */
-    func incrementBulletCount() {
+    // Increment the player's amount of coins
+    func incrementCoinCount() {
         
-        bullets = bulletCount() + 1
+        coins = coinCount() + 1
         saveData()
         
     }
@@ -145,15 +178,23 @@ class Upgrade {
         
     }
     
-    // Increment the player's amount of coins
-    func incrementCoinCount() {
+    // Increment the player's amount of piercing cost
+    func incrementPiercingCostCount() {
         
-        coins = coinCount() + 1
+        piercingCost = piercingCostCount() + 50
         saveData()
         
     }
     
-    // Increment the player's amount of coins
+    /** Increment the player's number of bullets */
+    func incrementBulletCount() {
+        
+        bullets = bulletCount() + 1
+        saveData()
+        
+    }
+    
+    // Increment the player's amount of bullet cost
     func incrementBulletCostCount() {
         
         bulletCost = bulletCostCount() + 50
@@ -161,10 +202,18 @@ class Upgrade {
         
     }
     
-    // Increment the player's amount of coins
-    func incrementPiercingCostCount() {
+    // Increment the player's radius ability
+    func incrementRadiusCount() {
         
-        piercingCost = piercingCostCount() + 50
+        radius = radiusCount() + 1
+        saveData()
+        
+    }
+    
+    // Increment the player's amount of piercing cost
+    func incrementRadiusCostCount() {
+        
+        radiusCost = radiusCostCount() + 50
         saveData()
         
     }
